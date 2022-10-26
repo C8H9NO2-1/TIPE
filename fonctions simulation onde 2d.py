d71a343367f9c6f78f09523c063364cbebce37dc
marioux9 <102533922+marioux9@users.noreply.github.com>
2022-10-26 12:18:55 +0200

    Rajout des conditions de Neumann

--- fonctions simulation onde 2d.py
+++ fonctions simulation onde 2d.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 
+def enfiler(e,f):
+    f.append(e)
 
-def resolutionEq2d(cube, Nt, delta_x, delta_y, delta_t, c): #besoin des primitives de files + module time
+def defiler(f):
+    return f.pop(0)
+
+
+def resolutionEq2d(cube, Nt, delta_x, delta_y, delta_t, c): #modifie le cube passé en paramètres
 
     """
     cube : cube avec conditions initiales
@@ -24,17 +30,53 @@ def resolutionEq2d(cube, Nt, delta_x, delta_y, delta_t, c): #besoin des primitiv
         temps1 = time.time()
         cube[1:-1,1:-1,t+1] = 2*cube[1:-1,1:-1,t]-cube[1:-1,1:-1,t-1]+((delta_t*c)**2)*((cube[2:,1:-1,t]-2*cube[1:-1,1:-1,t]+cube[:-2,1:-1,t])/(delta_x)**2+(cube[1:-1,2:,t]-2*cube[1:-1,1:-1,t]+cube[1:-1,:-2,t])/(delta_y**2))
 
-def condIni(Nx, Ny, r_init, xcenter, ycenter, decalX, decalY):
+
+def resolutionEq2DNeumann(cube, Nt, delta_x, delta_y, delta_t, c): #modifie le cube passé en paramètres (conditions de Neumann) simule 4murs autour de l'onde
+
+    """
+    cube : cube avec conditions initiales
+    mur(cube) : fonction qui impose
+    Nt : nombre de valeurs de temps sur lesquelles résoudre l'eq de propag de l'onde
+    delta_x : distance entre x et x+1
+    delta_y : ditance entre y et y+1
+    delta_t : durée entre t et t+1
+    c : célérité de l'onde
+    """
+    Nx = len(cube[:,0,0])
+    Ny = len(cube[0,:,0])
+
+    file=[0 for _ in range(10)] #le temps moyen sera basé sur 10 itération
+    temps1 = time.time()
+    for t in range(1,Nt-1):
+        enfiler(time.time()-temps1,file)
+        defiler(file) #pour garder 10 valeurs dans la file
+        print(np.mean(file)*(Nt-t)/60) #calcul du temps restant à partir des 10 val dans la file (renvoie le temps restant en minutes)
+        temps1 = time.time()
+        cube[1:-1,1:-1,t+1] = 2*cube[1:-1,1:-1,t]-cube[1:-1,1:-1,t-1]+((delta_t*c)**2)*((cube[2:,1:-1,t]-2*cube[1:-1,1:-1,t]+cube[:-2,1:-1,t])/(delta_x)**2+(cube[1:-1,2:,t]-2*cube[1:-1,1:-1,t]+cube[1:-1,:-2,t])/(delta_y**2))
+        for x in range(1,Nx-1):
+            cube[x,1,t+1]=cube[x,2,t+1]
+            cube[x,0,t+1]=cube[x,1,t+1]
+            cube[x,Ny-2,t+1]=cube[x,Ny-3,t+1]
+            cube[x,Ny-1,t+1]=cube[x,Ny-2,t+1]
+        for y in range(1,Ny-1):
+            cube[1,y,t+1]=cube[2,y,t+1]
+            cube[0,y,t+1]=cube[1,y,t+1]
+            cube[Nx-2,y,t+1]=cube[Nx-3,y,t+1]
+            cube[Nx-1,y,t+1]=cube[Nx-2,y,t+1]
+
+def condIni(Nx, Ny, Nt, r_init, xcenter, ycenter, decalX, decalY): #cos**2 en 2d qui se déplace (problème de stabilité lorsque (decalX,decalY) != (1,0) )
 
     """
     Nx : nombre de valeurs d'abscisse
     Ny : nombre de valeurs d'ordonnée
+    Nt : nombre de valeurs de temps
     r_init : rayon d'action de la fonction donnant les valeurs initiales
     xcenter : coord x du centre du cercle d'action de la fonction
     ycenter : idem en y
     decalX : décalage de l'onde initiale en x entre t=0 et t=1
     decalY : idem en y
     """
+    cube = np.zeros((Nx,Ny,Nt))
     nb_ang = int(1.5 * 2*np.pi*r_init) # calcul permetant de parcourir tous les indices sur le rayon r_init avec une "marge" de 1.5
     angles = np.linspace(0,2*np.pi,nb_ang)
 
@@ -44,9 +86,10 @@ def condIni(Nx, Ny, r_init, xcenter, ycenter, decalX, decalY):
             cube[int(np.cos(theta)*r)+xcenter,int(np.sin(theta)*r)+ycenter,0] = np.cos(r/(2*r_init)*np.pi)
     for x in range(1,Nx):
         for y in range(Ny):
-            cube[x,y,1] = cube[x-1,y,0]
+            cube[x,y,1] = cube[x-decalX,y-decalY,0]
+    return cube
 
-def dimQual(nouvNx, nouvNy, cube): #diminue la qualité du cube pour un affichage plus fluide
+def dimQual(nouvNx, nouvNy, cube): #diminue la qualité du cube pour un affichage plus fluide, renvoie un nouveau cube et les coordonnées associées à chaque point (le point cube[x,y,t] a pour coordonnées (Xtab[x],Ytab[y],cube[x,y,t]) dans le repère (O,Ox,Oy,Oz)
 
     """
     nouvNx : nombre de valeurs en x à afficher
@@ -54,23 +97,30 @@ def dimQual(nouvNx, nouvNy, cube): #diminue la qualité du cube pour un affichag
     cube : cube sur lequel se baser
     """
 
-    temp = np.zeros(nouvNx,nouvNy,len(cube[0,0,:]))
-    pasX = int((Nx-2)/nouvNx) #nombre d'indices qui seront "sautés" pour donner le nouveau cube
-    pasY = int((Ny-2)/nouvNy) # on prend en compte un indice tous les "pasY"
+    Nx = len(cube[:,0,0])
+    Ny = len(cube[0,:,0])
+
+    temp = np.zeros((nouvNx,nouvNy,len(cube[0,0,:])))
+
+    Xtab = np.linspace(0,Nx-1,nouvNx,dtype = "int")
+    Ytab = np.linspace(0,Ny-1,nouvNy,dtype = "int")
+    print(Xtab[0])
     for x in range(nouvNx):
         for y in range(nouvNy):
-            temp[x,y,:] = cube[pasX * x, pasY * y,:]
-    return temp
+            temp[x,y,:] = cube[Xtab[x], Ytab[y],:]
+    Xtab, Ytab = np.meshgrid(Xtab,Ytab)
+    return temp, Xtab, Ytab
 
 
-def affCube(cube, fps, frn, nbX, nbY, Xlim, Ylim, Zlim_bas, Zlim_haut):
+
+def affCube(cube, Xtab, Ytab, fps, frn, Xlim, Ylim, Zlim_bas, Zlim_haut): #affiche le cube entré en paramètres
 
     """
     cube : cube à afficher
+    Xtab : tableau des abscisses (l'indice i de Xtab correspond à l'abscisse du point "cube[i,:,t]" avec t quelconque)
+    Ytab : idem avec l'ordonnée
     fps : nombre d'images par seconde
     frn : nombre de frames (souvent le nombre de valeurs de temps qu'on veut afficher)
-    nbX : nombre de pt d'abscisse à afficher (souvent Nx)
-    nbY : nombre de pt d'ordonnée à afficher (souvent Ny)
     Xlim : limite de graduation de l'axe X
     Ylim : limite de graduation de l'axe Y
     Zlim_bas : limite basse de graduation de l'axe Z
@@ -79,10 +129,6 @@ def affCube(cube, fps, frn, nbX, nbY, Xlim, Ylim, Zlim_bas, Zlim_haut):
 
     plt.rcParams["figure.figsize"] = [7.50, 3.50]
     plt.rcParams["figure.autolayout"] = True
-
-    Xtab = np.arange(0,nbX,1)
-    Xtab, Ytab = np.meshgrid(Xtab, Xtab) #crée un plan pour le plot
-
 
     def change_plot(frame_number, cube, plot):
         plot[0].remove()
@@ -103,4 +149,3 @@ def affCube(cube, fps, frn, nbX, nbY, Xlim, Ylim, Zlim_bas, Zlim_haut):
 
     ani = animation.FuncAnimation(fig, change_plot, frn, fargs=(cube, plot), interval=1000 / fps)
     plt.show()
-
