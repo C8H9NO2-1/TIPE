3433222d499bc1984d0b3c51b35b0513693944e1
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2022-12-15 16:00:32 +0100

    Changes

--- new.py
+++ new.py
@@ -81,114 +81,131 @@ f1 = 5e-2
 
 omega0 = 2 * np.pi * f0
 omega1 = 2 * np.pi * f1
-A0 = 15
-A1 = 5e-2
+A0 = 1
+A1 = 1
 k0 = omega0 / c
 k1 = omega1 / c
 lambda0 = c / f0
 
 
-x0 = 250
-y0 = 150
+x0 = 150
+y0 = 0
+h = 150
+epsilon = 0.01
 
 M = np.zeros((Nx, Ny, Nt))
 
-file = [0 for _ in range(10)] #le temps moyen sera basé sur 10 itération
-temps1 = time.time()
-
-for t in range(Nt - 1, Nt):
-    enfiler(time.time()-temps1,file)
-    defiler(file) #pour garder 10 valeurs dans la file
-    # print(np.mean(file)*(Nt-t)/60) #calcul du temps restant à partir des 10 val dans la file
-    temps1 = time.time()
-    
-    for x in range(Nx - int(lambda0) - 1, Nx):
-        for y in range(150):
-            r = np.sqrt((x0 - x)**2 + (y0 - y)**2)
-            
-            if x <= c * t:
-                M[x][y][t] = A1 * np.cos(omega1 * t - k1 * x)
-                
-            if r <= c * t and r != 0:
-                M[x][y][t] += A0 * np.cos(omega0 * t + k0 * r) / r
-                
-t = Nt - 1
-mean = 0
-m = 0
-
-for y in range(150):
-    for x in range(Nx - int(lambda0) - 1, Nx):
-        if M[x][y][t] > m:
-            m = M[x][y][t]
-    mean += m
-
-print(mean / 150 / A1)
-
-#!=================================================================================================
-#? Code permettant de déterminer les signaux à chaque instant
-
 # file = [0 for _ in range(10)] #le temps moyen sera basé sur 10 itération
 # temps1 = time.time()
+
 # for t in range(Nt):
 #     enfiler(time.time()-temps1,file)
 #     defiler(file) #pour garder 10 valeurs dans la file
 #     print(np.mean(file)*(Nt-t)/60) #calcul du temps restant à partir des 10 val dans la file
 #     temps1 = time.time()
     
-#     for x in range(Nx):
-#         for y in range(150):
-#             r = np.sqrt((x0 - x)**2 + (y0 - y)**2)
-            
+#     for x in range(0, Nx):
+#         for y in range(h):
 #             if x <= c * t:
 #                 M[x][y][t] = A1 * np.cos(omega1 * t - k1 * x)
-                
+            
+#     for x in range(max(0, x0 - c * t), min(Nx, x0 + c * t)):
+#         for y in range(min(c * t, int(A0 / epsilon))):
+#             r = np.sqrt((x0 - x)**2 + (y0 - y)**2)
 #             if r <= c * t and r != 0:
-#                 M[x][y][t] += A0 * np.cos(omega0 * t + k0 * r) / r
+#                 alpha = y // h
+#                 if alpha % 2 == 0:
+#                     M[x][y - alpha * h][t] += A0 * np.cos(omega0 * t - k0 * r) / r
+#                 else:
+#                     M[x][(1 + alpha) * h - y][t] += A0 * np.cos(omega0 * t - k0 * r) / r
+                
+
 
 # cubeaff, Xtab, Ytab = dimQual(50,50,M)
 # affCube(cubeaff,Xtab,Ytab,250,Nt,Nx,Ny,-0.5,1.5)
 
 #!=================================================================================================
+#? Code permettant de déterminer une approximation de l'atténuation
+
+# file = [0 for _ in range(10)] #le temps moyen sera basé sur 10 itération
+# temps1 = time.time()
+# for t in range(Nt - 1, Nt):
+#     enfiler(time.time()-temps1,file)
+#     defiler(file) #pour garder 10 valeurs dans la file
+#     # print(np.mean(file)*(Nt-t)/60) #calcul du temps restant à partir des 10 val dans la file
+#     temps1 = time.time()
+    
+#     for x in range(Nx - int(lambda0) - 1, Nx):
+#         for y in range(h):
+#             if x <= c * t:
+#                 M[x][y][t] = A1 * np.cos(omega1 * t - k1 * x)
+            
+#     for x in range(max(0, x0 - c * t), min(Nx, x0 + c * t)):
+#         for y in range(min(c * t, int(A0 / epsilon))):
+#             r = np.sqrt((x0 - x)**2 + (y0 - y)**2)
+#             if r <= c * t and r != 0:
+#                 alpha = y // h
+#                 if alpha % 2 == 0:
+#                     M[x][y - alpha * h][t] += A0 * np.cos(omega0 * t - k0 * r) / r
+#                 else:
+#                     M[x][(1 + alpha) * h - y][t] += A0 * np.cos(omega0 * t - k0 * r) / r
+
+# t = Nt - 1
+# mean = 0
+# m = 0
+
+# for y in range(150):
+#     for x in range(Nx - int(lambda0) - 1, Nx):
+#         if M[x][y][t] > m:
+#             m = M[x][y][t]
+#     mean += m
+
+# print(mean / 150 / A1)
+
+#!=================================================================================================
 #? Code permettant de déterminer le déphasage optimal
 
-# I = np.linspace(0, Nx, Nx)
-# rapports = np.zeros(Nx)
+I = np.linspace(0, Nx, Nx)
+rapports = np.zeros(Nx)
 
-# for i in range(Nx):
-#     file = [0 for _ in range(10)] #le temps moyen sera basé sur 10 itération
-#     temps1 = time.time()
-#     for t in range(Nt - 1, Nt):
-#         enfiler(time.time()-temps1,file)
-#         defiler(file) #pour garder 10 valeurs dans la file
-#         print(np.mean(file)*(Nt-t)/60) #calcul du temps restant à partir des 10 val dans la file
-#         temps1 = time.time()
+for i in range(100):
+    file = [0 for _ in range(10)] #le temps moyen sera basé sur 10 itération
+    temps1 = time.time()
+    for t in range(Nt - 1, Nt):
+        enfiler(time.time()-temps1,file)
+        defiler(file) #pour garder 10 valeurs dans la file
+        # print(np.mean(file)*(Nt-t)/60) #calcul du temps restant à partir des 10 val dans la file
+        temps1 = time.time()
         
-#         for x in range(Nx - int(lambda0) - 1, Nx):
-#             for y in range(150):
-#                 r = np.sqrt((x0 - x)**2 + (y0 - y)**2)
+        for x in range(Nx - int(lambda0) - 1, Nx):
+            for y in range(h):
+                if x <= c * t:
+                    M[x][y][t] = A1 * np.cos(omega1 * t - k1 * x)
                 
-#                 if x <= c * t:
-#                     M[x][y][t] = A1 * np.cos(omega1 * t - k1 * x)
-                    
-#                 if r <= c * t and r != 0:
-#                     M[x][y][t] += A0 * np.cos(omega0 * t + k0 * r) / r
+        for x in range(Nx - int(lambda0) - 1, Nx):
+            for y in range(min(c * t, int(A0 / epsilon))):
+                r = np.sqrt((x0 - x)**2 + (y0 - y)**2)
+                if r <= c * t and r != 0:
+                    alpha = y // h
+                    value = A0 * np.cos(omega0 * t - k0 * r + i / 100 * np.pi) / r
+                    if alpha % 2 == 0:
+                        M[x][y - alpha * h][t] += value
+                    else:
+                        M[x][(1 + alpha) * h - y][t] += value
 
-#     # cubeaff, Xtab, Ytab = dimQual(50,50,M)
-#     # affCube(cubeaff,Xtab,Ytab,250,Nt,Nx,Ny,-0.5,1.5)
+    t = Nt - 1
+    mean = 0
+    m = 0
 
-#     t = Nt - 1
-#     mean = 0
-#     m = 0
-
-#     for y in range(150):
-#         for x in range(Nx - int(lambda0) - 1, Nx):
-#             if M[x][y][t] > m:
-#                 m = M[x][y][t]
-#         mean += m
+    for y in range(150):
+        for x in range(Nx - int(lambda0) - 1, Nx):
+            if M[x][y][t] > m:
+                m = M[x][y][t]
+        mean += m
         
-#     rapports[i] = mean / 150 / A1
+    rapports[i] = mean / 150 / A1
 
-# plt.plot(I, rapports)
-# plt.show()
+plt.plot(I, rapports)
+plt.show()
 
 #!=================================================================================================
