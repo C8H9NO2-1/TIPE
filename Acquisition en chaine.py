5ac796229165f06ce5f6902378e8dfa57cc100aa
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2023-03-09 14:02:47 +0100

    Changes

--- Acquisition en chaine.py
+++ Acquisition en chaine.py
@@ -4,6 +4,21 @@ import matplotlib.pyplot as plt
 import time
 import scipy.optimize
 
+sys = pyc.Sysam("SP5")
+
+delta_i = 400
+
+f = 400
+Te = 1/(f * delta_i)
+
+t = np.linspace(0, 1/f, delta_i + 1)
+Amp1 = 10
+Amp2 = 1
+signal1 = np.array([Amp1 * np.sin(i / delta_i * 2 * np.pi) for i in range(delta_i + 1)])
+
+Te_entree = 1 / (1000 * f) # periode d'echantillonage des entrees
+nb_points = 50000
+
 def fit_sin(tt, yy):
     '''Fit sin to the input time sequence, and return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"'''
     # tt = np.array(tt)
@@ -22,17 +37,55 @@ def fit_sin(tt, yy):
     fitfunc = lambda t: A * np.sin(w*t + p) + c
     return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": np.max(pcov), "rawres": (guess,popt,pcov)}
 
-sys = pyc.Sysam("SP5")
+def valeur_signal_primaire():
+    pyc.Sysam.config_sortie(sys, 1, Te * 10**6, signal1, -1)
+    
+    pyc.Sysam.config_entrees(sys, [0], [0.4])
+    pyc.Sysam.config_echantillon(sys, Te_entree * 10**6, nb_points)
+    
+    amplitudes = []
+    frequences = []
+    
+    for _ in range(10):
+        pyc.Sysam.declencher_sorties(sys, 1, 0)
+        
+        time.sleep(2/340) # Le temps que le signal atteigne le bout du tuyau
+        
+        pyc.Sysam.acquerir(sys)
+        temps = pyc.Sysam.temps(sys)
+        tensions = pyc.Sysam.entrees(sys)
+        
+        temp = temps[0]
+        tension = tensions[0]
+        temps = temp[:nb_points - int(0.02 / Te_entree)]
+        tension = tension[int(0.02 / Te_entree):]
+        
+        regression = fit_sin(temp, tension)["fitfunc"](temp)
+        
+        amplitudes.append(regression["amp"])
+        frequences.append(regression["freq"])
+        
+        pyc.Sysam.stopper_sorties(sys, 1, 1)
 
-delta_i = 400
+    amplitudes = np.array(amplitudes)
+    frequences = np.array(frequences)
+    amp_mean = np.mean(amplitudes)
+    freq_mean = np.mean(frequences)
+    amp_std = np.std(amplitudes)
+    freq_std = np.std(frequences)
+    
+    print("amp_mean: ", amp_mean, "\n", "freq_mean: ", freq_mean, "\n", "amp_std: ", amp_std, "\n", "freq_std: ", freq_std, "\n")
+    
+    array = np.array([amp_mean, freq_mean, amp_std, freq_std])
+    np.save("signal primaire.npy", array)
+    
+# valeur_signal_primaire()
+signal_primaire = np.load("signal primaire.npy") 
 
-f = 400
-Te = 1/(f * delta_i)
+def test_phases(tab_phases):
+    
+    
 
-t = np.linspace(0, 1/f, delta_i + 1)
-Amp1 = 10
-Amp2 = 1
-signal1 = np.array([Amp1 * np.sin(i / delta_i * 2 * np.pi) for i in range(delta_i + 1)])
 
 
 
@@ -46,22 +99,19 @@ signal1 = np.array([Amp1 * np.sin(i / delta_i * 2 * np.pi) for i in range(delta_
 pyc.Sysam.config_sortie(sys, 1, Te * 10**6, signal1, -1)
 pyc.Sysam.config_sortie(sys, 2, Te * 10**6, signal2, -1)
 
-Te_entree = 1 / (1000 * f) # periode d'echantillonage des entrees
-nb_points = 50000
-
 pyc.Sysam.config_entrees(sys, [0], [0.4])
 pyc.Sysam.config_echantillon(sys, Te_entree * 10**6, nb_points)
 
 # Acquisition du signal primaire
-pyc.Sysam.declencher_sorties(sys, 1, 0)
+# pyc.Sysam.declencher_sorties(sys, 1, 0)
 
-pyc.Sysam.acquerir(sys)
+# pyc.Sysam.acquerir(sys)
 
-temps = pyc.Sysam.temps(sys)
-tensions = pyc.Sysam.entrees(sys)
+# temps = pyc.Sysam.temps(sys)
+# tensions = pyc.Sysam.entrees(sys)
 
-temp1 = temps[0]
-tension1 = tensions[0]
+# temp1 = temps[0]
+# tension1 = tensions[0]
 
 # Acquisition du signal primaire accompagne du signal secondaire
 tab_temp2 = []
@@ -88,12 +138,12 @@ for j in range(36):
     pyc.Sysam.stopper_sorties(sys, 1, 1)
 
 # On tronque les tableaux pour eleminer le debut (on retire les points sur les 0.02 premieres secondes
-temp1 = temp1[:nb_points - int(0.02 / Te_entree)]
-tension1 = tension1[int(0.02 / Te_entree):]
+# temp1 = temp1[:nb_points - int(0.02 / Te_entree)]
+# tension1 = tension1[int(0.02 / Te_entree):]
 temp2 = temp2[:nb_points - int(0.02 / Te_entree)]
 tension2 = tension2[int(0.02 / Te_entree):]
 
-regression1 = fit_sin(temp1, tension1)["fitfunc"](temp1)
+# regression1 = fit_sin(temp1, tension1)["fitfunc"](temp1)
 regression2 = fit_sin(temp2, tension2)["fitfunc"](temp2)
 
 #===========================================================
@@ -104,18 +154,18 @@ regression2 = fit_sin(temp2, tension2)["fitfunc"](temp2)
 #plt.legend()
 
 # On tronque l'affichage
-m = int(len(temp1) / 2)
-temp1 = temp1[m:m + int(2 * 1/(f * Te_entree))]
-tension1 = tension1[m:m + int(2 * 1/(f * Te_entree))]
+m = int(len(temp2) / 2)
+# temp1 = temp1[m:m + int(2 * 1/(f * Te_entree))]
+# tension1 = tension1[m:m + int(2 * 1/(f * Te_entree))]
 temp2 = temp2[m:m + int(2 * 1/(f * Te_entree))]
 tension2 = tension2[m:m + int(2 * 1/(f * Te_entree))]
-regression1 = regression1[m:m + int(2 * 1/(f * Te_entree))]
+# regression1 = regression1[m:m + int(2 * 1/(f * Te_entree))]
 regression2 = regression2[m:m + int(2 * 1/(f * Te_entree))]
 
-plt.figure()
-plt.plot(temp1, tension1, label = "Source primaire", marker="x", ls="None")
-plt.plot(temp1, regression1, label = "Source primaire regression")
-plt.legend()
+# plt.figure()
+# plt.plot(temp1, tension1, label = "Source primaire", marker="x", ls="None")
+# plt.plot(temp1, regression1, label = "Source primaire regression")
+# plt.legend()
 
 plt.figure()
 plt.plot(temp2, tension2, label = "Source primaire + source secondaire", marker="x", ls="None")
@@ -123,7 +173,7 @@ plt.plot(temp2, regression2, label = "Source primaire + source secondaire regres
 plt.legend()
 
 plt.figure()
-plt.plot(temp1, regression1, label = "Source primaire regression")
+# plt.plot(temp1, regression1, label = "Source primaire regression")
 plt.plot(temp2, regression2, label = "Source primaire + source secondaire regression")
 plt.legend()
 
